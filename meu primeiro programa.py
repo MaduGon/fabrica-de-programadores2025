6121dff3d4ee3c5dde503f3da5c140f2fea44da7
MaduGon <mariaeduardagon03@gmail.com>
2025-05-22 15:26:57 -0300

    Meu primeiro programa em Python
    Aula sobre os primeiros passos em Python.

---
+++ meu primeiro programa.py
@@ -0,0 +1,51 @@
+
+#nome="maria eduarda"
+#numero = input()
+#print(numero)
+
+#salario = 14000
+#aumento = 65
+#print (salario + (salario * aumento / 100))
+
+#print(10 + 20 * 30)
+#(4**2 / 30)
+#(9**4 + 2) * (6 - 1)
+#
+#a = 4
+#b = 10
+#c = 5.0
+#d = 1 
+#f = 5
+#
+#print(4 == 5.0)
+#print(4 > 10)
+#print(c > f)
+#print(1 < 10)
+#print(4 == 10)
+#print(5.0 < 1)
+#print(10 > 4)
+#print(5.0 >= 5)
+#print(5 >= 5.0)
+#print(5.0 <= 5.0)
+#print(5.0 <= 5
+
+L = []     
+soma = 0
+while True:
+    n=int(input("digete um número inteiro:")) 
+    if n == 0:
+        break
+    
+    L.append(n)    
+    x=0
+while x < len(L):  
+    print(L[x]) 
+    x=x+1 
+
+
+
+
+
+
+
+                  
