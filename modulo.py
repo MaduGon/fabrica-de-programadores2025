f71df9d5d3e6f9b20d5d256b0c933dca2ed59798
MaduGon <mariaeduardagon03@gmail.com>
2025-06-05 15:30:27 -0300

    Add files via upload

---
+++ modulo.py
@@ -0,0 +1,12 @@
+import random
+value = random.randint(0, 100)
+
+'''match value:
+    case 0:
+        print("zero!")
+    case 1:
+        print("um!")
+    case 2:
+        print("dois!")
+'''
+print(value)
